70a01597012897409e0a20d641520a32241ee42a
fatemeh <sfmodarres@gmail.com>
2017-03-17 15:01:53 -0400

    initial method to compare two clusterings with and without orphaned reads.

---
+++ diffExp/compareActPred.py
@@ -0,0 +1,40 @@
+def compareActPred(fn_clust):
+    clusts = ({}, {})
+    contigs = ({}, {})
+    i = 0
+    # read both cluster files for two experiments (mag.flat.clust)
+    for fn in fn_clust:
+        with open(fn) as fp:
+            for line in fp:
+                tup = line.strip().split() #tup[0]:cluster_id, tup[1]:contig
+                if tup[0] not in clusts[i]:
+                    clusts[i][tup[0]] = []
+                clusts[i][tup[0]] += [tup[1]] # cluster -> list of contigs
+                contigs[i][tup[1]]=tup[0] # contig -> cluster
+        i += 1
+    # build the (bipartite) graph
+    vertices = set()
+    edges = {}
+    for i in [0,1]:
+        for clust, conts in clusts[i].iteritems():
+            clust_name0 = str(i) + clust
+            vertices.add(clust_name0)
+            for contig in conts:
+                clust_name1 = str(1-i) + contigs[1-i][contig]
+                key = (clust_name0, clust_name1)
+                if key not in edges:
+                    edges[key] = 0
+                edges[key] += 1
+    return vertices, edges
+
+import argparse
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Enter the address to all your files.')
+    parser.add_argument('dirAddr', type=str, help='the address to the directory containing all files')
+    args = parser.parse_args()
+    clust_file1 = args.dirAddr + "mag.flat.clust.orig"
+    clust_file2 = args.dirAddr + "mag.flat.clust.orphan"
+    clust_files = [clust_file1, clust_file2]
+    vertices, edges = compareActPred(clust_files)
+    import pdb; pdb.set_trace()
+    print(len(vertices))
